1778ca99ccc4621cfb1a449c6e6d0a13c2149d61
Viviane <106716101+profvivi@users.noreply.github.com>
2026-08-12 17:12:41 -0300

    s

--- script.js
+++ script.js
@@ -53,6 +53,9 @@ let perguntaAtual;
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
+function mostraAlternativas() {}
 
 mostraPergunta();
+
