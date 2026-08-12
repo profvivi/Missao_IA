b1e1a60a615e39bf81863362e96e2b4403f3924d
Viviane <106716101+profvivi@users.noreply.github.com>
2026-08-12 17:23:02 -0300

    s

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ const perguntas = [
   {
     enunciado:
       "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-    alternativas: 
+    alternativas: [
     "Isso é assustador!", 
     "Isso é maravilhoso!",
+    ],
   },
   {
     enunciado:
